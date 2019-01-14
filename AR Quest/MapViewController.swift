bf8482db9ef402d17171d22116f1bd5bdf687c43
Denis Bystruev <dbystruev@me.com>
2019-01-14 18:06:09 +0300

    Added Khoroshkola location

--- AR Quest/MapViewController.swift
+++ AR Quest/MapViewController.swift
@@ -6,9 +6,16 @@
 //  Copyright © 2018 Denis Bystruev. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
     var prizes: [PrizeObject]?
+    
+    let prize = PrizeObject(
+        coordinate: CLLocationCoordinate2D(latitude: 55.77, longitude: 37.47),
+        title: "Хорошкола",
+        subtitle: "Народного ополчения, 9А"
+    )
 }
 
